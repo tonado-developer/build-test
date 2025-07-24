4f0e72c3159edafd3f9daa667b3c8d9c282791be
Programmierung BW <134686002+tonado-developer@users.noreply.github.com>
2025-07-24 16:45:16 +0200

    test build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. NICHT 23
+            Save and see your changes instantly. NICHT Nichts
           </li>
         </ol>
 
