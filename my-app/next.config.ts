bb9badf58f22f6a3291c91f0cea2e190d722cf57
Programmierung BW <134686002+tonado-developer@users.noreply.github.com>
2025-07-24 15:39:47 +0200

    local node

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isDDEV = process.env.DDEV_SITENAME;
+const allowedOrigins = isDDEV ? [
+  `127.0.0.1`,
+  `${process.env.DDEV_SITENAME}.ddev.site`,
+  `https://${process.env.DDEV_SITENAME}.ddev.site`
+] : [];
+
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
+  allowedDevOrigins: allowedOrigins,
 };
 
 export default nextConfig;
