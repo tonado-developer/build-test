6b5a77b0d2d8122bccc6f29eb74db9c1d4ceaa9d
Programmierung BW <134686002+tonado-developer@users.noreply.github.com>
2025-07-24 17:34:27 +0200

    bugfix

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
 
-const isDDEV = process.env.DDEV_SITENAME;
-const allowedOrigins = isDDEV ? [
-  `127.0.0.1`,
-  `${process.env.DDEV_SITENAME}.ddev.site`,
-  `https://${process.env.DDEV_SITENAME}.ddev.site`
-] : [];
-
 const nextConfig: NextConfig = {
-  allowedDevOrigins: allowedOrigins,
-  ...(process.env.NODE_ENV === 'production' && {
-    output: 'export',
-    trailingSlash: true,
-    images: { unoptimized: true }
-  }),
   images: {
-    domains: ['https://sandbox.programmierung-bw.de'], // WordPress Domain hinzufügen
+    domains: ['sandbox.programmierung-bw.de'],
   },
 };
 
