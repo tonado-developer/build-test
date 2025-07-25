cbe8aea34550ec01bd825d0660a4d8f1d9df4d9a
Programmierung BW <134686002+tonado-developer@users.noreply.github.com>
2025-07-25 08:27:04 +0200

    bugfix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  ...(process.env.NODE_ENV === 'production' && {
+    output: 'export',
+    trailingSlash: true,
+  }),
   images: {
     domains: ['sandbox.programmierung-bw.de'],
+    unoptimized: true
   },
 };
 
