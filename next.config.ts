1a9c3ced36753835bed9d9526541c0d156b109ae
Programmierung BW <134686002+tonado-developer@users.noreply.github.com>
2025-07-25 08:41:20 +0200

    rollback

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ...(process.env.DEPLOY_TARGET === 'static' && {
-    output: 'export',
-    trailingSlash: true,
-  }),
   images: {
-    ...(process.env.DEPLOY_TARGET === 'static' 
-      ? { unoptimized: true }
-      : { domains: ['sandbox.programmierung-bw.de'] }
-    ),
+    domains: ['sandbox.programmierung-bw.de'],
   },
 };
 
